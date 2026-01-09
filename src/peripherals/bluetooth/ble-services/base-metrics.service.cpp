6fee3bca050c4678ee7d66a9207408209566aa40
Double-A-92 <2192982+Double-A-92@users.noreply.github.com>
2026-01-09 18:35:03 +0100

    Use ternaries and don't mess with the present bits

--- src/peripherals/bluetooth/ble-services/base-metrics.service.cpp
+++ src/peripherals/bluetooth/ble-services/base-metrics.service.cpp
@@ -143,31 +143,24 @@ void BaseMetricsBleService::ftmsTask(void *parameters)
 {
     {
         const auto *const params = static_cast<const BaseMetricsBleService::BaseMetricsParams *>(parameters);
-        unsigned short presentFeatures = FTMSSensorBleFlags::ftmsMeasurementFeaturesFlag;
 
         const auto secInMicroSec = 1e6L;
         const auto dragFactor = static_cast<unsigned char>(lround(params->data.dragCoefficient * 1e6));
 
-        unsigned char strokeRate = 0;
         const auto strokeTimeDelta = ((params->data.strokeTime - params->data.previousStrokeTime) / secInMicroSec / 60U);
-        if (strokeTimeDelta > 0L)
-            strokeRate = static_cast<unsigned char>(lroundl((params->data.strokeCount - params->data.previousStrokeCount) / strokeTimeDelta));
+        const unsigned char strokeRate = strokeTimeDelta > 0L ? lroundl((params->data.strokeCount - params->data.previousStrokeCount) / strokeTimeDelta) : 0U;
 
-        unsigned short pace500m = 0;
         const auto revTimeDelta = ((params->data.revTime - params->data.previousRevTime) / secInMicroSec);
         const auto distanceDelta = ((params->data.distance - params->data.previousDistance) / 100U);
-        if (distanceDelta > 0L)
-            pace500m = static_cast<unsigned short>(lroundl(500U * (revTimeDelta / distanceDelta)));
-        else
-            presentFeatures &= ~FTMSMeasurementFeaturesFlags::InstantaneousPacePresent;
+        const unsigned short pace500m = distanceDelta > 0L ? lroundl(500U * (revTimeDelta / distanceDelta)) : 0U;
 
         const auto distance = static_cast<unsigned int>(lround(params->data.distance / 100U));
         const auto avgStrokePower = static_cast<short>(lround(params->data.avgStrokePower));
 
         const auto length = 14U;
         array<unsigned char, length> temp = {
-            static_cast<unsigned char>(presentFeatures),
-            static_cast<unsigned char>(presentFeatures >> 8),
+            static_cast<unsigned char>(FTMSSensorBleFlags::ftmsMeasurementFeaturesFlag),
+            static_cast<unsigned char>(FTMSSensorBleFlags::ftmsMeasurementFeaturesFlag >> 8),
 
             // Stroke rate is with a resolution of 0.5. While this works for a rower it will not work for a kayak erg in all cases (as kayak stroke rate can be up to 160spm)
             static_cast<unsigned char>(strokeRate * 2 > UCHAR_MAX ? UCHAR_MAX : strokeRate * 2),
